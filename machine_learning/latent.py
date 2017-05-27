84aef0650b8dfa40105049571e2c68c9c097d26f
Handason <handasontam@gmail.com>
2017-05-28 02:57:00 +0800

    update latent factor model

--- machine_learning/latent.py
+++ machine_learning/latent.py
@@ -14,7 +14,7 @@ class LatentFactorModel():
              [0,0,0,9.35,0,0,0,6.29,17.41,6.34,0,0,0,0,4.83,4.61,0,0,0,0,0,0,0,0,10.4,0,0,15.56,0,0,0,0,0,0,0,0,0,7.23,17.96,0,0,0,0,0,0,0,0,0,0,0],
              [8.71,0,0,0,0,0,0,0,0,0,1.23,0,3.2,2.02,0,0,0.73,0.95,1.09,2,2.26,0.37,5.15,0,0,33.03,2.12,0,0,3.53,5.14,0,16.76,1.12,0,1.27,2.06,0,0,0,0,1.31,2.68,1.93,0,1.37,0,0,0,0]
             ]
-        print(numpy.shape(numpy.array(self.R)))
+        # print(numpy.shape(numpy.array(self.R)))
         self.df = pd.DataFrame(self.R)
 
         self.R = numpy.array(self.R)
@@ -26,11 +26,7 @@ class LatentFactorModel():
         self.P = numpy.random.rand(N,self.K)
         self.Q = numpy.random.rand(M,self.K)
         self.nP, self.nQ = self.matrix_factorization(self.R, self.P, self.Q, self.K)
-        self.nQ_T = numpy.transpose(self.nQ)
-        approximate_matrix = numpy.dot(self.nP, self.nQ_T)
-        self.approximate_df = pd.DataFrame(approximate_matrix)
 
-        numpy.savetxt('approximate_matrix.csv', approximate_matrix, delimiter=',')
 
 
     """
@@ -77,10 +73,14 @@ class LatentFactorModel():
     #     print('last approximation error: ', previous_e)
         self.nP = P
         self.nQ = Q.T
+        self.nQ_T = numpy.transpose(self.nQ)
+        approximate_matrix = numpy.dot(self.nP, self.nQ_T)
+        self.approximate_df = pd.DataFrame(approximate_matrix)
+        numpy.savetxt('approximate_matrix.csv', approximate_matrix, delimiter=',')
         return P, Q.T
 
     def predict(self, i):
-        print(self.approximate_df[self.df==0].iloc[i].sort_values(ascending=False, na_position = 'last').to_frame().T.iloc[:,0:5])
+        return(self.approximate_df[self.df==0].iloc[i].sort_values(ascending=False, na_position = 'last').to_frame().T.iloc[:,0:5])
 
 
     ###############################################################################
@@ -88,7 +88,9 @@ class LatentFactorModel():
 if __name__ == "__main__":
 
     model = LatentFactorModel()
-    prediction = model.predict(0) # return top 5 with index
-    model.R[0][10] = 0.5
+    prediction = model.predict(1) # return top 5 with index
+    print(prediction.columns.values)
+    model.R[0][10] = 1000
     model.update()
-    new_prediction = model.predict(0)
+    new_prediction = model.predict(1)
+    print(new_prediction.columns.values)
